2b2ed0340d19f5bbee1c6751d9529adeb1439c37
Azure99 <961523404@qq.com>
2021-03-02 13:05:34 +0800

    Update MainWindow.xaml.cs

--- src/View/MainWindow.xaml.cs
+++ src/View/MainWindow.xaml.cs
@@ -50,7 +50,7 @@ namespace GenshinPlayerQuery.View
                 return;
             }
 
-            WebBrowserMain.NavigateToString(Render.RenderHtml(playerQueryResult));
+            WebBrowserMain.NavigateToString(PlayerPageRender.RenderHtml(playerQueryResult));
             MessageBus.AddQueryHistory(uid);
         }
 
